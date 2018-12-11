5bcbe4d1a7c1e26750110f90a981305fda198ce1
aaron <469034277@qq.com>
2018-12-11 11:39:04 +0800

    update

---
+++ src/main/java/com/aaron/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.aaron.itoken.service.admin.mapper;
+
+import com.aaron.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
